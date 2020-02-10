198d3d4b861bab6c5755754935b9ab1f81ee54cf
Tom Hall <tfhall@protonmail.ch>
2020-02-10 18:06:28 +0000

    clear bug

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
-import { selectCartItems } from '../../redux/cart/cart.selections'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
 
 import './cart-dropdown.style.scss'
 
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux' 
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
-import { selectCartItmesCount } from '../../redux/cart/cart.selections'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
